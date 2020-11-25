1b75bc0682d7ed22b7b6f55fa852608569e80c0e
Vincent Ivos <vincentivos1@gmail.com>
2020-11-25 14:14:50 -0500

    commit tree bb117536f4a02c28df7762553cfe9bb4a6a6cd64

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
 class Student 
-
-end
+    class Student 
+        class Student < User
+          attr_accessor :knowledge
+        
+        end 
+          def initialize
+            @knowledge = []
+          end
+        
+          def learn(string)
+            knowledge << string
+          end
+        end
+    end 
